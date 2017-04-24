1a3051707db32033799ff3b42ceb67547ca448e1
Edward Hinkle <eddie@meetmaestro.com>
2017-04-23 20:57:33 -0400

    Initial Commit

---
+++ IndieAuth/IndieAuth.h
@@ -0,0 +1,19 @@
+//
+//  IndieAuth.h
+//  IndieAuth
+//
+//  Created by Eddie Hinkle on 4/23/17.
+//  Copyright © 2017 Studio H, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IndieAuth.
+FOUNDATION_EXPORT double IndieAuthVersionNumber;
+
+//! Project version string for IndieAuth.
+FOUNDATION_EXPORT const unsigned char IndieAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IndieAuth/PublicHeader.h>
+
+
