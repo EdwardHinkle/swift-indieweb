6d6a9b319baa91a321beead82baef0057b93d6ab
Edward Hinkle <eddie@meetmaestro.com>
2017-06-27 19:36:17 -0400

    Latest updates from indigenous

--- IndieAuth/IndieAuthLoginViewController.swift
+++ IndieAuth/IndieAuthLoginViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 import SafariServices
+import RelLinkParser
 
-public class IndieAuthLoginViewController: UIViewController,UITextFieldDelegate {
+let callbackUrl = URL(string: "indigenous://auth/callback")
+let appClientId = "http://indigenous.abode.pub"
+let indieAuthSetupUrl = "https://indieauth.com/setup"
+let kSafariViewControllerCloseNotification = "kSafariViewControllerCloseNotification"
+
+public class IndieAuthLoginViewController: UIViewController, UITextFieldDelegate, SFSafariViewControllerDelegate {
+    
+    var userEndpoints: [String : [URL]] = [:]
+    var userAccessToken: String? = nil
+    var userScope: String? = nil
     
     enum IndieWebEndpointType: String {
         case Authorization = "authorization_endpoint"
@@ -18,30 +28,51 @@ public class IndieAuthLoginViewController: UIViewController,UITextFieldDelegate 
     }
     
     @IBOutlet weak var indieAuthDomain: UITextField?
+    @IBOutlet weak var progressDisplay: UIStackView!
+    @IBOutlet weak var loginDisplay: UIStackView!
+    @IBOutlet weak var domainInput: UITextField!
     
     @IBAction func readAboutIndieAuth(_ sender: UIButton) {
-        if let openUrl = URL(string: "https://indieauth.com/setup") {
-            let safariVC = SFSafariViewController(url: openUrl)
-            self.present(safariVC, animated: true, completion: nil)
+        if let openUrl = URL(string: indieAuthSetupUrl) {
+            DispatchQueue.main.sync {
+                let safariVC = SFSafariViewController(url: openUrl)
+                self.present(safariVC, animated: true, completion: nil)
+            }
         }
     }
     
     @IBAction func loginWithIndieAuth(_ sender: UIButton?) {
+        
+        loginDisplay?.isHidden = true
+        progressDisplay?.isHidden = false
     
         var url: URL? = nil
         
         if let urlDescription = indieAuthDomain?.text {
             // Take url from text field and convert it to a normalized URL
-            url = normalizeMeURL(url: urlDescription)
+            url = IndieAuth.normalizeMeURL(url: urlDescription)
         }
         
         if (url != nil) {
-            // URL is normalized, next step
-            discoverEndpoint(.Authorization, atUrl: url!) { endpointUrl in
-                if let authorizationEndpoint = endpointUrl {
-                    print("Authorization Endpoint Found \(authorizationEndpoint)")
-                } else {
-                    print("Authorization Endpoint Failed");
+            
+            // URL is normalized, next steps
+            RealLinkParser.getRelLinks(fromUrl: url!) { meEndpoints in
+                
+                // We need to save these endpoints for later use
+                self.userEndpoints = meEndpoints
+                print("User Endponts")
+                print(self.userEndpoints)
+                
+                if let authorizationEndpoints = meEndpoints["authorization_endpoint"] {
+                    let authorizationUrl = IndieAuth.buildAuthorizationURL(forEndpoint: authorizationEndpoints[0], meUrl: url!, redirectURI: callbackUrl!, clientId: appClientId, state: "Testing", scope: "read follow mute block create update")
+                    
+                    if let openUrl = authorizationUrl {
+                        DispatchQueue.main.sync {
+                            let safariVC = SFSafariViewController(url: openUrl)
+                            safariVC.delegate = self
+                            self.present(safariVC, animated: true, completion: nil)
+                        }
+                    }
                 }
             }
             
@@ -51,104 +82,73 @@ public class IndieAuthLoginViewController: UIViewController,UITextFieldDelegate 
         
     }
     
-    // Input: Any URL or string like "eddiehinkle.com"
-    // Output: Normlized URL (default to http if no scheme, default "/" path)
-    //         or return false if not a valid URL (has query string params, etc)
-    private func normalizeMeURL(url: String) -> URL? {
+    // Recieve a response URL from a authorization request or a token request. Process the url and call the appropriate functions to continue
+    public func indieAuthProcess(urlResponse: URL) {
+        var responseUrlComponents = URLComponents(url: urlResponse.absoluteURL, resolvingAgainstBaseURL: false)
+        var responseItems: [String: String] = [:]
+        var responseType: String? = nil
         
-        var meUrl = URLComponents(string: url)
-        
-        // If there is no scheme or host, the host is probably in the path
-        if (meUrl?.scheme == nil && meUrl?.host == nil) {
-            // If the path is nil or empty, then our url is probably empty. Mayday!
-            if (meUrl?.path == nil || meUrl?.path == "") {
-                return nil;
-            }
-            
-            // Split the path into segments so we can seperate the host and the path
-            let pathSegments = meUrl?.path.characters.split(separator: "/").map(String.init)
-            
-            meUrl?.host = pathSegments?.first;
-            meUrl?.path = "/" + (pathSegments?.dropFirst().joined() ?? "")
-        }
-        
-        // If no scheme, we default to http
-        if (meUrl?.scheme == nil) {
-            meUrl?.scheme = "http"
-        } else if (meUrl?.scheme != "http" && meUrl?.scheme != "https") {
-            // If there is a scheme, we only accept http and https schemes
-            print("Scheme existed and wasn't http or https: \(meUrl?.scheme ?? "No Scheme")")
-            return nil
-        }
-        
-        // We default to a path of /
-        if (meUrl?.path == nil || meUrl?.path == "") {
-            meUrl?.path = "/"
-        }
-        
-        // We don't want query or fragment messing up our url. Just set those to nil
-        meUrl?.fragment = nil
-        meUrl?.query = nil
-        
-        return meUrl?.url
-    }
-    
-    private func discoverEndpoint(_ endpointType: IndieWebEndpointType, atUrl meUrl: URL, completion: @escaping (URL?) -> ()) {
-        let request = URLRequest(url: meUrl)
-        
-        // set up the session
-        let config = URLSessionConfiguration.default
-        let session = URLSession(configuration: config)
-        
-        let task = session.dataTask(with: request) {
-            (data, response, error) in
-            // check for any errors
-            guard error == nil else {
-                print("error calling GET on \(meUrl)")
-                print(error ?? "No error present")
-                return
-            }
-            
-            // Check if endpoint is in the HTTP Header fields
-            if let httpResponse = response as? HTTPURLResponse {
-                print(httpResponse.allHeaderFields)
-                if let linkHeaderString = httpResponse.allHeaderFields["Link"] as? String {
-                    print("Link Headers Found")
-                    let linkHeaders = linkHeaderString.characters.split(separator: ",").map({charactersSequence in
-                        let matched = self.matches(for: "<([a-zA-Z:\\/\\.]+)>; rel=\"\(endpointType.rawValue)\"", in: String.init(charactersSequence))
-                        print(matched)
-//                        let header = charactersSequence.split(separator: "; rel=\"").map(String.init)
-//                        print (header)
-                    })
-                    
-                    print(linkHeaders)
-                    //endpointType.rawValue
-//                    if let headerUrl = URL(string: headerEndpoint) {
-//                        completion(headerUrl)
-                        completion(nil)
-                        return
-//                    }
-                    
+        if let items = responseUrlComponents?.queryItems {
+            for queryItem in items {
+                if let value = queryItem.value {
+                    if (queryItem.name == "code") {
+                        responseType = "code"
+                    } else if (queryItem.name == "access_token") {
+                        responseType = "access_token"
+                    }
+                    responseItems[queryItem.name] = value
                 }
             }
-
-            // Check if endpoint is in the HTML Head
-//            if let responseData = data {
-//                
-//            }
-            
-            
-            completion(nil)
         }
         
-         task.resume()
+        if let type = responseType {
+            switch type {
+            case "code":
+                if let meUrl = URL(string: responseItems["me"]!) {
+                    indieAuthProcess(authorizationCode: responseItems["code"]!, state: responseItems["state"] ?? "", meUrl: meUrl)
+                }
+            case "access_token":
+                print("Run Token")
+            default: break
+            }
+        }
     }
-
     
+    public func indieAuthProcess(authorizationCode: String, state: String, meUrl: URL) {
+        
+        let copyOfUserEndpoints = userEndpoints;
+        
+        if let tokenEndpoints = userEndpoints["token_endpoint"] {
+            IndieAuth.makeTokenRequest(forEndpoint: tokenEndpoints[0], meUrl: meUrl, code: authorizationCode, redirectURI: callbackUrl!, clientId: appClientId, state: state) { _, scope, accessToken in
+                
+                // Hide Safari View Controller
+                self.presentedViewController?.dismiss(animated: true, completion: nil)
+                
+                let micropubAuth = [
+                    "access_token": accessToken,
+                    "scope": scope,
+                    "me": meUrl.absoluteString,
+                    "microsub_endpoint": copyOfUserEndpoints["microsub"]?.first?.absoluteString ?? "",
+                    "micropub_endpoint": copyOfUserEndpoints["micropub"]?.first?.absoluteString ?? "",
+                    "authorization_endpoint": copyOfUserEndpoints["authorization_endpoint"]?.first?.absoluteString ?? "",
+                    "token_endpoint": copyOfUserEndpoints["token_endpoint"]?.first?.absoluteString ?? ""
+                ]
+                
+                let defaults = UserDefaults(suiteName: "group.software.studioh.indigenous")
+                defaults?.set(micropubAuth, forKey: "micropubAuth")
+                
+                DispatchQueue.main.sync {
+                    self.dismiss(animated: true, completion: nil)
+                    self.progressDisplay?.isHidden = true
+                    self.loginDisplay?.isHidden = false
+                }
+                
+            }
+        }
+        
+    }
     
-    
-    
-    
+    // Mark: View Controller Functions
     
     public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
@@ -159,6 +159,7 @@ public class IndieAuthLoginViewController: UIViewController,UITextFieldDelegate 
     public override func viewDidLoad() {
         super.viewDidLoad()
         indieAuthDomain?.delegate = self
+        domainInput?.becomeFirstResponder()
     }
 
     public override func didReceiveMemoryWarning() {
@@ -173,9 +174,18 @@ public class IndieAuthLoginViewController: UIViewController,UITextFieldDelegate 
             let regex = try NSRegularExpression(pattern: regex)
             let nsString = text as NSString
             let results = regex.matches(in: text, range: NSRange(location: 0, length: nsString.length))
-            // This currently returns the entire match, this needs to be modified to return only the captured groups
-            // look more into this https://code.tutsplus.com/tutorials/swift-and-regular-expressions-swift--cms-26626
-            return results.map { nsString.substring(with: $0.range)}
+            var matches: [String] = []
+            for match in results {
+                for n in 1..<match.numberOfRanges {
+                    let range = match.rangeAt(n)
+                    let r = text.index(text.startIndex, offsetBy: range.location) ..< text.index(text.startIndex, offsetBy: range.location+range.length)
+                    matches.append(text.substring(with: r))
+                }
+            }
+            
+            
+            
+            return matches
         } catch let error {
             print("invalid regex: \(error.localizedDescription)")
             return []
